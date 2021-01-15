fab7d631ed0c5f03675f78783ebf84344b9ffa1e
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-01-15 12:04:44 +0300

    Исправлена проблеама прокрутки сайта под меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 const pageHeader = document.querySelector('.header');
 const headerToggle = document.querySelector('.menu__toggle');
+const body = document.querySelector('.page__body')
 
 pageHeader.classList.remove('header--no-js');
 pageHeader.classList.add('header--closed');
@@ -9,8 +10,10 @@ headerToggle.addEventListener('click', function () {
   if (pageHeader.classList.contains('header--closed')) {
     pageHeader.classList.remove('header--closed');
     pageHeader.classList.add('header--opened');
+    body.style = 'overflow: hidden'
   } else {
     pageHeader.classList.add('header--closed');
     pageHeader.classList.remove('header--opened');
+    body.style = 'overflow: visible'
   }
 });
