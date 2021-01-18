38bacf510d977623591b6695bf49b23b2f81f4b5
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-01-18 11:48:21 +0300

    Исправлены ошибки eslinter

--- source/js/map.js
+++ source/js/map.js
@@ -1,25 +1,25 @@
 'use strict';
-  ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-        center: [59.938635, 30.323118],
-        zoom: 16
-      }, {
-        searchControlProvider: 'yandex#search'
-      }),
-      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-      ),
+ymaps.ready(function () {
+  const myMap = new ymaps.Map('map', {
+      center: [59.938635, 30.323118],
+      zoom: 16
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
 
-      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-        hintContent: 'Магазин MooN',
-        balloonContent: 'Магазин велосипедов MooN'
-      }, {
-        iconLayout: 'default#image',
-        iconImageHref: 'img/fav32.png',
-        iconImageSize: [32, 32],
-        iconImageOffset: [-16, -16]
-      });
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: 'Магазин MooN',
+      balloonContent: 'Магазин велосипедов MooN'
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/fav32.png',
+      iconImageSize: [32, 32],
+      iconImageOffset: [-16, -16]
+    });
 
-    myMap.geoObjects
-      .add(myPlacemark)
-  });
+  myMap.geoObjects
+    .add(myPlacemark);
+});
