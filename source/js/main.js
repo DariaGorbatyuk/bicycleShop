639d8ac4268e9da71ef0a2852afed693dd088ea7
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-01-20 11:19:31 +0300

    Добавлено IIFE

--- source/js/main.js
+++ source/js/main.js
@@ -1,21 +1,23 @@
-const pageHeader = document.querySelector('.header');
-const menu = document.querySelector('.menu');
-const headerToggle = menu.querySelector('.menu__toggle');
-const body = document.querySelector('.page__body')
+(() => {
+  const pageHeader = document.querySelector('.header');
+  const menu = document.querySelector('.menu');
+  const headerToggle = menu.querySelector('.menu__toggle');
+  const body = document.querySelector('.page__body')
 
-pageHeader.classList.remove('header--no-js');
-pageHeader.classList.add('header--closed');
+  pageHeader.classList.remove('header--no-js');
+  pageHeader.classList.add('header--closed');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('header--closed')) {
-    pageHeader.classList.remove('header--closed');
-    pageHeader.classList.add('header--opened');
-    body.style = 'overflow: hidden';
-    menu.style = 'overflow: auto';
-  } else {
-    pageHeader.classList.add('header--closed');
-    pageHeader.classList.remove('header--opened');
-    body.style = 'overflow: visible';
-    menu.style = '';
-  }
-});
+  headerToggle.addEventListener('click', function () {
+    if (pageHeader.classList.contains('header--closed')) {
+      pageHeader.classList.remove('header--closed');
+      pageHeader.classList.add('header--opened');
+      body.style = 'overflow: hidden';
+      menu.style = 'overflow: auto';
+    } else {
+      pageHeader.classList.add('header--closed');
+      pageHeader.classList.remove('header--opened');
+      body.style = 'overflow: visible';
+      menu.style = '';
+    }
+  });
+})()
